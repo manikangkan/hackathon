5cb8fef8e513642ab767cd730b638e4107bf4437
manikangkandas <manikangkan.das@outlook.com>
2022-06-19 13:04:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-3xl">Hi</h2>
+      <h1 className="text-3xl font-mono">Manikangkan</h1>
+    </div>
+  );
+}
+
+export default App;
